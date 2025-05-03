9b65d51c81d0e3cc0c07d2cec8d8768beec49d9c
Gola-k <kunalgola9930@gmail.com>
2025-05-03 16:37:38 +0530

    new localhost added

--- backend/app.py
+++ backend/app.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Add your frontend URL here
+    allow_origins=["http://localhost:5173", "http://localhost:5174"],  # Add your frontend URL here
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
